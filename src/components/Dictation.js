a0ae35d1347c3434c8638bd34d0137dba08d5a1f
Anusha <anusha19murthy@gmail.com>
2026-06-04 01:04:26 +0530

    fixed useEffect issues

--- src/components/Dictation.js
+++ src/components/Dictation.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef} from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './Navbar';
